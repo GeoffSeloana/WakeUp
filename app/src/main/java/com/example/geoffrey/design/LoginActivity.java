b5fe2dbe30af9ae478b07053ee99e1c86eff4091
geoffrey <geoffrey.seloana@gmail.com>
2017-07-14 00:53:35 +0200

    design

---
+++ app/src/main/java/com/example/geoffrey/design/LoginActivity.java
@@ -0,0 +1,8 @@
+package com.example.geoffrey.design;
+
+/**
+ * Created by geoffrey on 7/14/17.
+ */
+
+public class LoginActivity {
+}
--- app/src/main/java/com/example/geoffrey/design/SplashActivity.java
+++ app/src/main/java/com/example/geoffrey/design/SplashActivity.java
@@ -1,17 +1,14 @@
 package com.example.geoffrey.design;
 
-import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import mehdi.sakout.fancybuttons.FancyButton;
-
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash_activity);
 
 
     }
