0b6279b9050693710f642a102e0b7c059599981d
geoffrey <geoffrey.seloana@gmail.com>
2017-07-16 00:01:43 +0200

    design

--- app/src/main/java/com/example/geoffrey/design/LoginActivity.java
+++ app/src/main/java/com/example/geoffrey/design/LoginActivity.java
@@ -1,8 +1,25 @@
 package com.example.geoffrey.design;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+
 /**
  * Created by geoffrey on 7/14/17.
  */
 
-public class LoginActivity {
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.login_activity);
+
+    }
+
 }
--- app/src/main/java/com/example/geoffrey/design/SplashActivity.java
+++ app/src/main/java/com/example/geoffrey/design/SplashActivity.java
@@ -1,15 +1,33 @@
 package com.example.geoffrey.design;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
 
 public class SplashActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.splash_activity);
 
+        final View btn_getStarted = findViewById(R.id.btn_getStarted);
 
+        btn_getStarted.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(SplashActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 }
