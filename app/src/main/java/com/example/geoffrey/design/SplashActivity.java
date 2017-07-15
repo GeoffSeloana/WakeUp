80940258d1ed75ad5de7470dbc03a37d638f5962
geoffrey <geoffrey.seloana@gmail.com>
2017-07-16 00:41:45 +0200

    design

--- app/src/main/java/com/example/geoffrey/design/SplashActivity.java
+++ app/src/main/java/com/example/geoffrey/design/SplashActivity.java
@@ -1,12 +1,15 @@
 package com.example.geoffrey.design;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.RelativeLayout;
+
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -17,6 +20,7 @@ public class SplashActivity extends AppCompatActivity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.splash_activity);
+
 
         final View btn_getStarted = findViewById(R.id.btn_getStarted);
 
